e5fba0abf4db178b0e0e1a4b419f08774de08503
Shreyas Jani <janishreyas5@gmail.com>
2024-01-30 21:51:46 +0530

    db schema, fix bugs, Book Feedback

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ currentDirectory = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 api = Api(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{os.path.join(currentDirectory, "database.sqlite3")}'
-app.config["SECRET_KEY"] = "thisissecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{os.path.join(currentDirectory, "db.sqlite3")}'
+app.config["SECRET_KEY"] = "PsIK>@%=`TiDs$>"
 
 db = SQLAlchemy()
 login_manager = LoginManager(app)
@@ -19,106 +19,111 @@ login_manager = LoginManager(app)
 db.init_app(app)
 app.app_context().push()
 
-
-class UserModel(db.Model, UserMixin):
-    __tablename__ = "user"
+class UserLoginModel(db.Model, UserMixin):
+    __tablename__ = "user_login"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
-
-class InfoModel(db.Model):
-    __tablename__ = "info"
-    username = db.Column(db.String(30), db.ForeignKey("user.username"), primary_key=True)
-    first_name = db.Column(db.String(30), nullable=False)
-    last_name = db.Column(db.String(30))
-    role = db.Column(db.String(30), nullable=False)
+class UserInfoModel(db.Model):
+    __tablename__ = "user_info"
+    username = db.Column(db.String(40), db.ForeignKey("user_login.username"), primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20))
+    role = db.Column(db.String(20), nullable=False)
 
 class SectionModel(db.Model):
     __tablename__ = "section"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False)
     description = db.Column(db.String(100), nullable=False)
 
 class BookModel(db.Model):
-    __tablename__ = 'book'
+    __tablename__ = "book"
     isbn = db.Column(db.String(13), primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    content = db.Column(db.String(500), nullable=False)
-    section_id = db.Column(db.Integer, db.ForeignKey('section.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(300), nullable=False)
+    section_id = db.Column(db.Integer, db.ForeignKey("section.id"), nullable=False)
 
-class BookAuthorsModel(db.Model):
-    __tablename__ = 'book_authors'
-    isbn = db.Column(db.String(13), db.ForeignKey('book.isbn'), primary_key=True)
-    author_name = db.Column(db.String(30), primary_key = True)
+class BookAuthorModel(db.Model):
+    __tablename__ = "book_author"
+    isbn = db.Column(db.String(13), db.ForeignKey("book.isbn"), primary_key=True)
+    author_name = db.Column(db.String(40), primary_key = True)
 
 class BookRequestsModel(db.Model):
-    __tablename__ = 'book_requests'
-    isbn = db.Column(db.String(13), db.ForeignKey('book.isbn'), primary_key=True)
-    username = db.Column(db.String(30), db.ForeignKey('user.username'), primary_key=True)
-    request_date = db.Column(db.DateTime, nullable=False)
+    __tablename__ = "book_request"
+    isbn = db.Column(db.String(13), db.ForeignKey("book.isbn"), primary_key=True)
+    username = db.Column(db.String(40), db.ForeignKey('user_login.username'), primary_key=True)
+    date_of_request = db.Column(db.DateTime, nullable=False)
 
 class BookIssueModel(db.Model):
-    __tablename__ = 'book_issue'
-    isbn = db.Column(db.String(13), db.ForeignKey('book.isbn'), primary_key=True)
-    username = db.Column(db.String(30), db.ForeignKey('user.username'), primary_key=True)
+    __tablename__ = "book_issue"
+    isbn = db.Column(db.String(13), db.ForeignKey("book.isbn"), primary_key=True)
+    username = db.Column(db.String(40), db.ForeignKey("user_login.username"), primary_key=True)
     date_of_issue = db.Column(db.DateTime, nullable=False)
     date_of_return = db.Column(db.DateTime, nullable=False)
 
-parser = reqparse.RequestParser()
-parser.add_argument("first_name", type=str)
-parser.add_argument("last_name", type=str)
+class BookFeedbackModel(db.Model):
+    __tablename__ = "book_feedback"
+    username = db.Column(db.String(40), db.ForeignKey("user_login.username"), primary_key = True)
+    isbn = db.Column(db.String(13), db.ForeignKey("book.isbn"), primary_key = True)
+    feedback = db.Column(db.String(500), nullable = False)
 
-parser.add_argument("username", type=str)
-parser.add_argument("password", type=str)
-parser.add_argument("role", type=str)
+reqParser = reqparse.RequestParser()
 
-parser.add_argument("isbn", type=str)
-parser.add_argument("book_name", type=str)
-parser.add_argument("section_name", type=str)
-parser.add_argument("description", type=str)
-parser.add_argument("content", type=str)
+reqParser.add_argument("isbn", type=str)
+reqParser.add_argument("book_name", type=str)
+reqParser.add_argument("section_name", type=str)
+reqParser.add_argument("description", type=str)
+reqParser.add_argument("content", type=str)
+reqParser.add_argument("author_names", type=str)
 
-parser.add_argument("author_names", type=str)
+reqParser.add_argument("username", type=str)
+reqParser.add_argument("password", type=str)
+reqParser.add_argument("first_name", type=str)
+reqParser.add_argument("last_name", type=str)
+reqParser.add_argument("role", type=str)
+
+reqParser.add_argument("feedback", type = str)
 
 
 def check_role(role: str):
     def decorator(function):
         def wrapper(*args, **kwargs):
-            info = InfoModel.query.filter_by(username=current_user.username).first()
+            info = UserInfoModel.query.filter_by(username=current_user.username).first()
 
-            if not info or info.role != role:
-                return {"message": f"Only {role} can access this endpoint"}, 400
+            if not info:
+                return {"message": "Info not found"}, 404
+
+            if info.role != role:
+                return {"message": f"Only {role} user has access here"}, 400
             else:
                 return function(*args, **kwargs)
-
         return wrapper
-
     return decorator
-
 
 @login_manager.user_loader
 def load_user(id):
-    return UserModel.query.get(id)
+    return UserLoginModel.query.get(id)
 
 class AddUser(Resource):
     def post(self):
-        args = parser.parse_args()
+        args = reqParser.parse_args()
         try:
-            first_name = args["first_name"]
-            last_name = args["last_name"]
             username = args["username"]
             password = args["password"]
+            first_name = args["first_name"]
+            last_name = args["last_name"]
             role = args["role"]
 
             if role not in ["Librarian", "General"]:
                 return {"message": "Role must be either Librarian or General"}, 400
 
-            user = UserModel(username=username, password=password)  # type: ignore
-            db.session.add(user)
+            userLogin = UserLoginModel(username=username, password=password)  # type: ignore
+            db.session.add(userLogin)
 
-            info = InfoModel(username=user.username, first_name=first_name, last_name=last_name, role=role)  # type: ignore
+            info = UserInfoModel(username=userLogin.username, first_name=first_name, last_name=last_name, role=role)  # type: ignore
             db.session.add(info)
             db.session.commit()
 
@@ -130,7 +135,7 @@ class AddUser(Resource):
 
 class Login(Resource):
     def post(self):
-        args = parser.parse_args()
+        args = reqParser.parse_args()
         try:
             username = args["username"]
             password = args["password"]
@@ -139,11 +144,11 @@ class Login(Resource):
             if role not in ["Librarian", "General"]:
                 return {"message": "Role must be either Librarian or General"}, 400
 
-            this_user = UserModel.query.filter_by(username=username).first()
+            userLogin = UserLoginModel.query.filter_by(username=username).first()
 
-            if this_user:
+            if userLogin:
                 # User exists
-                info = InfoModel.query.filter_by(username=this_user.username).first()
+                info = UserInfoModel.query.filter_by(username=userLogin.username).first()
 
                 if not info:
                     return {"message": "Info does not exist"}, 404
@@ -151,9 +156,9 @@ class Login(Resource):
                 if info.role != role:
                     return {"message": f"User is not a {role} user"}, 400
 
-                if this_user.password == password:
+                if userLogin.password == password:
                     # Password matches
-                    login_user(this_user)
+                    login_user(userLogin)
                     return {
                         "message": f"{role} user Login successful. Welcome {info.first_name}"
                     }, 200
@@ -167,21 +172,22 @@ class Login(Resource):
         except Exception as e:
             return {"Error": f"{e}"}, 500
 
-
 class UserInfo(Resource):
     @login_required
     def get(self):
-        user = UserModel.query.filter_by(username=current_user.username).first()
+        userLogin = UserLoginModel.query.filter_by(username=current_user.username).first()
 
-        if not user:
+        if not userLogin:
             return {"message": "User does not exist"}, 404
 
-        info = InfoModel.query.filter_by(username=user.username).first()
+        info = UserInfoModel.query.filter_by(username=userLogin.username).first()
 
         if not info:
             return {"message": "User does not exist"}, 404
 
-        return {"username": user.username, "password": user.password, "first_name": info.first_name, "last_name": info.last_name, "role": info.role}, 200
+        last_name = info.last_name if info.last_name else ""
+
+        return {"username": userLogin.username, "password": userLogin.password, "first_name": info.first_name, "last_name": last_name, "role": info.role}, 200
 
 
 class Logout(Resource):
@@ -193,7 +199,7 @@ class AddSection(Resource):
     @login_required
     @check_role(role = "Librarian")
     def post(self):
-        args = parser.parse_args()
+        args = reqParser.parse_args()
         try:
             name = args["section_name"]
             description = args["description"]
@@ -214,11 +220,11 @@ class AddBookAuthor(Resource):
     @check_role(role = "Librarian")
     def post(self):
         try:
-            args = parser.parse_args()
+            args = reqParser.parse_args()
             isbn = args["isbn"]
             book_name = args["book_name"]
             section_name = args["section_name"]
-            content = args["content"]
+            content_path = args["content"]
             author_names: str = args["author_names"]
             author_names_list: List[str] = author_names.split(",")
 
@@ -232,13 +238,13 @@ class AddBookAuthor(Resource):
             if book:
                 return {"message": "Book already exists"}, 400
             
-            book = BookModel(isbn=isbn, name=book_name, content=content, section_id=section.id)  # type: ignore
+            book = BookModel(isbn=isbn, name=book_name, content=content_path, section_id=section.id)  # type: ignore
             db.session.add(book)
 
             for author_name in author_names_list:
-                author = BookAuthorsModel(isbn=isbn, author_name=author_name)  # type: ignore
+                author = BookAuthorModel(isbn=isbn, author_name=author_name)  # type: ignore
                 db.session.add(author)
-                db.session.commit()
+            db.session.commit()
 
             return {"message": f"Book {book_name} with authors {author_names} added successfully"}, 201
 
@@ -249,12 +255,12 @@ class ViewSections(Resource):
     @login_required
     def get(self):
         try:
-            sections = SectionModel.query.all()
-
+            sections = list(SectionModel.query.all())
+            
             if not sections:
-                return {"message": "No sections exist"}, 404
-
-            return {"sections": [section.name for section in sections]}, 200
+                return {"message": "No section exists"}, 404
+            
+            return {"sections": [(section.id, section.name, str(section.date_created), section.description) for section in sections]}, 200
 
         except Exception as e:
             return {"Error": f"{e}"}, 500
@@ -268,15 +274,15 @@ class ViewBooks(Resource):
             if not books:
                 return {"message": "No book exists"}, 404
             
-            book_sections = []
+            outputList = []
             for book in books:
-                section = SectionModel.query.filter_by(id=book.section_id).first()
+                section = SectionModel.query.filter_by(id = book.section_id).first()
                 if not section:
-                    return {"message": "Section does not exist"}, 404
+                    return {"message": "Section not found"}, 404
                 
-                book_sections.append((book.name, section.name))
-
-            return {"books": [book_section for book_section in book_sections]}, 200
+                outputList.append((book.isbn, book.name, book.content, book.section_id, section.name))
+            
+            return {"Books": outputList}
         
         except Exception as e:
             return {"Error": f"{e}"}, 500
@@ -285,7 +291,7 @@ class RequestBook(Resource):
     @login_required
     @check_role(role = "General")
     def post(self):
-        args = parser.parse_args()
+        args = reqParser.parse_args()
         try:
             isbn = args["isbn"]
             book = BookModel.query.filter_by(isbn=isbn).first()
@@ -293,7 +299,7 @@ class RequestBook(Resource):
             if not book:
                 return {"message": "Book does not exist"}, 404
             
-            book_request = BookRequestsModel(isbn=isbn, username=current_user.username, request_date=datetime.now())  # type: ignore
+            book_request = BookRequestsModel(isbn=isbn, username=current_user.username, date_of_request=datetime.now())  # type: ignore
             db.session.add(book_request)
             db.session.commit()
 
@@ -306,7 +312,7 @@ class IssueBook(Resource):
     @login_required
     @check_role(role = "Librarian")
     def post(self):
-        args = parser.parse_args()
+        args = reqParser.parse_args()
         try:
             isbn = args["isbn"]
             username = args["username"]
@@ -321,8 +327,12 @@ class IssueBook(Resource):
 
             BookRequestsModel.query.filter_by(isbn=isbn, username=username).delete()
             db.session.commit()
+            
+            book = BookModel.query.filter_by(isbn = isbn).first()
+            if not book:
+                return {"message": "Book does not exist"}, 404
 
-            return {"message": f"Book {isbn} issued to {username} successfully"}, 201
+            return {"message": f"Book {book.name} issued to {username} successfully"}, 201
 
         except Exception as e:
             return {"Error": f"{e}"}, 500
@@ -331,11 +341,11 @@ class ReturnBook(Resource):
     @login_required
     @check_role(role = "General")
     def post(self):
-        args = parser.parse_args()
+        args = reqParser.parse_args()
         try:
             isbn = args["isbn"]
             username = current_user.username
-            book_issue = BookIssueModel.query.filter_by(isbn = isbn, username = username)
+            book_issue = BookIssueModel.query.filter_by(isbn = isbn, username = username).first()
             
             if not book_issue:
                 return {"message": f"Book with isbn {isbn} is not issued by user {username}"}, 404
@@ -343,10 +353,42 @@ class ReturnBook(Resource):
             BookIssueModel.query.filter_by(isbn = isbn, username = username).delete()
             db.session.commit()
 
-            return {"message": f"Book with isbn {isbn} has been returned"}, 200
+            book = BookModel.query.filter_by(isbn = isbn).first()
+            if not book:
+                return {"message": "Book does not exist"}, 404
+
+            return {"message": f"Book {book.name} has been returned"}, 200
         
         except Exception as e:
             return {"Error": f"{e}"}, 500
+
+class BookFeedback(Resource):
+    @login_required
+    @check_role(role = "General")
+    def post(self):
+        args = reqParser.parse_args()
+        try:
+            isbn = args["isbn"]
+            feedback = args["feedback"]
+            
+            username = current_user.username
+
+            book_feedback = BookFeedbackModel(isbn = isbn, username = username, feedback = feedback)  # type: ignore
+            db.session.add(book_feedback)
+            db.session.commit()
+
+            book = BookModel.query.filter_by(isbn = isbn).first()
+            if not book:
+                return {"message": "Book does not exist"}, 404
+            
+            return {"message": f"Feedback for {book.name} submitted successfully"}, 200
+
+        except Exception as e:
+            return {"Error": f"{e}"}, 500
+
+# class EditBookInfo(Resource):
+
+
 
 api.add_resource(AddUser, "/api/addUser")
 api.add_resource(Login, "/api/login")
@@ -359,6 +401,7 @@ api.add_resource(ViewBooks, '/api/viewBooks')
 api.add_resource(RequestBook, '/api/requestBook')
 api.add_resource(IssueBook, '/api/issueBook')
 api.add_resource(ReturnBook, "/api/returnBook")
+api.add_resource(BookFeedback, "/api/bookFeedback")
 
 if __name__ == "__main__":
     app.run(debug=True)
